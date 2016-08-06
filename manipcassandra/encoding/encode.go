7e03686c63d96805f3b04227f4ab91cbac19bb57
Antoine Mercadal <antoine.mercadal@gmail.com>
2016-08-05 17:17:23 -0700

    fixed: encode empty string, slices, 0 numbers etc.

--- manipcassandra/encoding/encode.go
+++ manipcassandra/encoding/encode.go
@@ -185,15 +185,7 @@ func PrimaryFieldsAndValues(val interface{}) ([]string, []interface{}, error) {
 
 func isEmptyValue(v reflect.Value) bool {
 	switch v.Kind() {
-	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
-		return v.Len() == 0
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return v.Int() == 0
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return v.Uint() == 0
-	case reflect.Float32, reflect.Float64:
-		return v.Float() == 0
-	case reflect.Interface, reflect.Ptr:
+	case reflect.Array, reflect.Map, reflect.Slice, reflect.Interface, reflect.Ptr:
 		return v.IsNil()
 	}
 
