2ab691e28cc591cb45ea17c5142fe15a6124089e
Tong Mu <dkwingsmt@gmail.com>
2016-12-15 17:21:20 -0800

    Add KeyValues in context.Parameters as http querystr. Add test.

--- parameters.go
+++ parameters.go
@@ -1,10 +1,12 @@
 package manipulate
 
-// Parameters is a parameter struct which can be used with Cassandra
+// Parameters is a parameter struct which can be used with Cassandra (fields)
+// or HTTP (KeyValues)
 type Parameters struct {
 	IfNotExists bool
 	IfExists    bool
 	UsingTTL    bool
 	OrderByDesc string
 	OrderByAsc  string
+	KeyValues   map[string]string
 }
