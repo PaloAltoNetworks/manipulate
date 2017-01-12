39f7e970350f6bc4e69c643b2cef5f8bdeff7931
Antoine Mercadal <antoine.mercadal@gmail.com>
2017-01-11 20:18:49 -0800

    new: manipulate.RetryManipulation

--- helpers.go
+++ helpers.go
@@ -3,6 +3,9 @@ package manipulate
 import (
 	"bytes"
 	"reflect"
+	"time"
+
+	"github.com/aporeto-inc/elemental"
 )
 
 // ConvertArrayToManipulables convert the given array of interface into an array of Manipulable
@@ -26,4 +29,55 @@ func WriteString(buffer *bytes.Buffer, str string) {
 	if _, err := buffer.WriteString(str); err != nil {
 		panic(err)
 	}
+}
+
+// RetryManipulation will retry the given function that tries a manipulate
+// operation at least maxTries if the error is a manipulate.ErrCannotCommunicate.
+// You can pass -1 to always retry. The function will retry immediately the first try,
+// then after 1s, 2s etc until a try every 5s.
+func RetryManipulation(manipulation func() error, onRetryFunc func(int), maxTries int) error {
+
+	try := 0
+	waitTime := 0 * time.Second
+
+	for {
+
+		err := manipulation()
+
+		// If the error is nil, its a success.
+		if err == nil {
+			break
+		}
+
+		// Check the type of the error.
+		e, ok := err.(elemental.Error)
+
+		// If it is not an elemental.Error, return it.
+		if !ok {
+			return err
+		}
+
+		// If it is an elemental.Error but the code is not ErrCannotCommunicate, return it.
+		if e.Code != ErrCannotCommunicate {
+			return err
+		}
+
+		// If we reach the maxtries, return the error
+		if maxTries != -1 && try == maxTries {
+			return err
+		}
+
+		if onRetryFunc != nil {
+			onRetryFunc(try)
+		}
+
+		// Otherwise wait, increase the time and try again.
+		<-time.After(waitTime)
+
+		if waitTime < 5*time.Second {
+			waitTime += 1 * time.Second
+		}
+	}
+
+	return nil
 }
