7ca9bf24dc2ed0ab97149b94078e44263f70c86d
Antoine Mercadal <antoine.mercadal@gmail.com>
2017-01-20 11:50:15 -0800

    new: added helper DoesDatabaseExist

--- manipmongo/helpers.go
+++ manipmongo/helpers.go
@@ -9,6 +9,28 @@ import (
 	mgo "gopkg.in/mgo.v2"
 )
 
+// DoesDatabaseExist checks if the database used by the given manipulator exists.
+func DoesDatabaseExist(manipulator manipulate.Manipulator) (bool, error) {
+
+	m, ok := manipulator.(*mongoManipulator)
+	if !ok {
+		return false, fmt.Errorf("You can only pass a Mongo Manipulator to CreateIndex")
+	}
+
+	dbs, err := m.session.DatabaseNames()
+	if err != nil {
+		return false, err
+	}
+
+	for _, db := range dbs {
+		if db == m.dbName {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
 // DropDatabase drops the entire database used by the given manipulator.
 func DropDatabase(manipulator manipulate.Manipulator) error {
 
