6600c24c702b21b7260cacc8b62ecb8f91e867fa
Antoine Mercadal <antoine.mercadal@gmail.com>
2017-02-27 09:40:47 -0800

    removed manipcassandra

--- manipcassandra/doc.go
+++
@@ -1,5 +0,0 @@
-// Package manipcassandra provides a Cassandra backed Manipulator.
-//
-// This package also provides a few helper fonctions to easily deal
-// with a Cassandra schema and keyspace.
-package manipcassandra
