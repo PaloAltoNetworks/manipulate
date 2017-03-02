abd23d7a0b641036c8aa5a9438632cc3e2f78ebb
Antoine Mercadal <antoine.mercadal@gmail.com>
2017-03-02 14:28:58 -0800

    Update doc.go

--- doc.go
+++ doc.go
@@ -6,14 +6,14 @@
 // which is an extension of the Manipulator add methods to manage transactions, like
 // Commit and Abort.
 //
-// A Manipulator works with a elemental.Identifiable.
+// A Manipulator works with some elemental.Identifiables.
 //
 // The storage engine used by a Manipulator is abstracted. By default manipulate
-// provides implementations for Cassandra, Mongo, ReST API. You can of course implement
-// Your own storage implementation.
+// provides implementations for Rest API over HTTP or websocket, Mongo DB, Memory and a mock Manipulator for
+// unit testing. You can of course create your own implementation.
 //
-// Each method of a Manipulator is taking a Context as argument. The context is used
-// to pass additional informations like a Filter, or some Parameters.
+// Each method of a Manipulator is taking a manipulate.Context as argument. The context is used
+// to pass additional informations like a Filter or some Parameters.
 //
 // Example for creating an object:
 //
@@ -22,15 +22,17 @@
 //      user.FullName, user.Login := "Antoine Mercadal", "primalmotion"
 //
 //      // Create Mongo Manipulator.
-//      m := manipmongo.NewMongoManipulator("127.0.0.1", "test")
+//      m := manipmongo.NewMongoManipulator([]{"127.0.0.1"}, "test", "db-username", "db-password", "db-authsource", 512)
 //
 //      // Then create the User.
-//      m.Create(nil, nil, user)
+//      m.Create(nil, user)
 //
 // Example for retreving an object:
 //
 //      // Create a Context with a filter.
-//      ctx := manipulate.NewContextWithFilter(manipulate.NewFilterComposer().WithKey("login").Equals("primalmotion"))
+//      ctx := manipulate.NewContextWithFilter(
+//          manipulate.NewFilterComposer().WithKey("login").Equals("primalmotion").
+//          Done())
 //
 //      // Retrieve the users matching the filter.
 //      var users models.UserLists
