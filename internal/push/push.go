ad92b8476173b5f65d65cf51dff2014c4cd87d0b
Dimitri Stiliadis <dstiliadis@gmail.com>
2018-01-09 18:36:09 -0800

    Fix timeout

--- internal/push/push.go
+++ internal/push/push.go
@@ -3,6 +3,8 @@ package push
 import (
 	"crypto/tls"
 	"errors"
+	"os"
+	"os/signal"
 	"sync"
 	"time"
 
@@ -121,6 +123,9 @@ func (s *subscription) connect(initial bool) (err error) {
 
 	try := 0
 
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
 	for {
 		s.conn, err = wsutils.Dial(
 			s.endpoint,
@@ -140,7 +145,12 @@ func (s *subscription) connect(initial bool) (err error) {
 			return err
 		}
 
-		time.Sleep(3 * time.Second)
+		select {
+		case <-time.After(3 * time.Second):
+		case <-c:
+			return manipulate.NewErrDisconnected("Disconnected per signal")
+		}
+
 	}
 }
 
