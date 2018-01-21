863ef7956d14e80207f508ccebc527970953b708
Antoine Mercadal <antoine.mercadal@gmail.com>
2018-01-20 16:41:11 -0800

    fixed: implementation

--- maniphttp/push.go
+++ maniphttp/push.go
@@ -1,18 +1,20 @@
 package maniphttp
 
 import (
+	"context"
+
 	"github.com/aporeto-inc/elemental"
 	"github.com/aporeto-inc/manipulate"
 	"github.com/aporeto-inc/manipulate/internal/push"
 )
 
 // NewSubscriber returns a new subscription.
-func NewSubscriber(manipulator manipulate.Manipulator, filter *elemental.PushFilter, recursive bool, maxConnTry int) (manipulate.Subscriber, error) {
+func NewSubscriber(ctx context.Context, manipulator manipulate.Manipulator, filter *elemental.PushFilter, recursive bool, maxConnTry int) (manipulate.Subscriber, error) {
 
 	m, ok := manipulator.(*httpManipulator)
 	if !ok {
 		panic("You must pass a HTTP manipulator to maniphttp.NewSubscriper")
 	}
 
-	return push.NewSubscriber(m.url, m.namespace, m.currentPassword(), m.tlsConfig, filter, recursive, maxConnTry)
+	return push.NewSubscriber(ctx, m.url, m.namespace, m.currentPassword(), m.tlsConfig, filter, recursive, maxConnTry)
 }
