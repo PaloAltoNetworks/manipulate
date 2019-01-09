fe475fb6a34d1e82aa1405386e01038a9c61c902
Antoine Mercadal <antoine.mercadal@gmail.com>
2019-01-08 19:31:45 -0800

    fixed: random lint issues

--- maniptest/manipulator_test.go
+++ maniptest/manipulator_test.go
@@ -6,7 +6,7 @@ import (
 
 	. "github.com/smartystreets/goconvey/convey"
 	"go.aporeto.io/elemental"
-	"go.aporeto.io/elemental/test/model"
+	testmodel "go.aporeto.io/elemental/test/model"
 	"go.aporeto.io/manipulate"
 )
 
