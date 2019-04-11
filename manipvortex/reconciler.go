3664749d64451aed99905e7a2c60b6df2163c323
Dimitri Stiliadis <dstiliadis@gmail.com>
2019-04-11 16:24:49 -0700

    Remove variadics

--- manipvortex/reconciler.go
+++ manipvortex/reconciler.go
@@ -17,18 +17,18 @@ type Reconciler interface {
 	// false, the objects are ignored.
 	// If it returns an error, the error will be forwarded to the caller.
 	// The Reconcile function may modify the objects to perform transformations.
-	Reconcile(manipulate.Context, elemental.Operation, ...elemental.Identifiable) (bool, error)
+	Reconcile(manipulate.Context, elemental.Operation, elemental.Identifiable) (bool, error)
 }
 
 // A TestReconciler is an Reconciler that can be used for
 // testing purposes.
 type TestReconciler interface {
 	Reconciler
-	MockReconcile(t *testing.T, impl func(manipulate.Context, elemental.Operation, ...elemental.Identifiable) (bool, error))
+	MockReconcile(t *testing.T, impl func(manipulate.Context, elemental.Operation, elemental.Identifiable) (bool, error))
 }
 
 type mockedReconcilerMethods struct {
-	reconcileMock func(manipulate.Context, elemental.Operation, ...elemental.Identifiable) (bool, error)
+	reconcileMock func(manipulate.Context, elemental.Operation, elemental.Identifiable) (bool, error)
 }
 
 type testReconciler struct {
@@ -46,13 +46,13 @@ func NewTestReconciler() TestReconciler {
 }
 
 // MockPrefetch sets the mocked implementation of Reconcile.
-func (p *testReconciler) MockReconcile(t *testing.T, impl func(manipulate.Context, elemental.Operation, ...elemental.Identifiable) (bool, error)) {
+func (p *testReconciler) MockReconcile(t *testing.T, impl func(manipulate.Context, elemental.Operation, elemental.Identifiable) (bool, error)) {
 	p.currentMocks(t).reconcileMock = impl
 }
 
-func (p *testReconciler) Reconcile(mctx manipulate.Context, op elemental.Operation, i ...elemental.Identifiable) (bool, error) {
+func (p *testReconciler) Reconcile(mctx manipulate.Context, op elemental.Operation, i elemental.Identifiable) (bool, error) {
 	if mock := p.currentMocks(p.currentTest); mock != nil && mock.reconcileMock != nil {
-		return mock.reconcileMock(mctx, op, i...)
+		return mock.reconcileMock(mctx, op, i)
 	}
 
 	return true, nil
