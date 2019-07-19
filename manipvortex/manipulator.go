8757b5263844751803ba80526e4a6b64ddc47a68
eric <eric@aporeto.com>
2019-07-19 16:09:32 -0700

    Update: Move dropping of messages into debug territory

--- manipvortex/manipulator.go
+++ manipvortex/manipulator.go
@@ -724,7 +724,7 @@ func (m *vortexManipulator) pushStatus(status manipulate.SubscriberStatus) {
 		select {
 		case s.subscriberStatusChannel <- status:
 		default:
-			zap.L().Error("Subscriber status channel is full")
+			zap.L().Debug("Dropping status as channel is full")
 		}
 	}
 }
@@ -738,7 +738,7 @@ func (m *vortexManipulator) pushErrors(err error) {
 		select {
 		case s.subscriberErrorChannel <- err:
 		default:
-			zap.L().Error("Subscriber error channel is full")
+			zap.L().Debug("Dropping error as channel is full")
 		}
 	}
 }
