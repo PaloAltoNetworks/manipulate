482424154999ef4d5ec81a8a92baef174a327bf0
Antoine Mercadal <antoine.mercadal@gmail.com>
2020-01-31 13:57:39 -0800

    fixed: remove debug print

--- maniphttp/manipulator.go
+++ maniphttp/manipulator.go
@@ -497,8 +497,6 @@ func (s *httpManipulator) prepareHeaders(request *http.Request, mctx manipulate.
 	if v := mctx.ClientIP(); v != "" {
 		request.Header.Set("X-Forwarded-For", v)
 	}
-
-	fmt.Println(request.Header)
 }
 
 func (s *httpManipulator) readHeaders(response *http.Response, mctx manipulate.Context) {
